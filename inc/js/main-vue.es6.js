7c1e5fcee30b8e928274b7106d7d156a400b8355
Salman <salmankaleem94@gmail.com>
2018-04-26 16:30:37 -0400

    Continued recreating functionality in Vue AJAX and user interaction functionality

--- inc/js/main-vue.es6.js
+++ inc/js/main-vue.es6.js
@@ -1,16 +1,68 @@
+const apiModule = (function(){
+  let module = {};
+  const url = "http://localhost:8888/api/index.php";
+
+  module.getUrl = () => {
+    return url;
+  };
+
+  return module;
+}());
+
 const vm = new Vue ({
   el: '#app',
   data: {
     minuteStart: 0,
     minuteEnd: 48,
-    currentDate: null,
+    currentDate: undefined,
+    gamesList: [],
+    selectedGameID: undefined,
   },
   methods: {
     getGames: function(event){
-      console.log( this.currentDate );
+      if( this.currentDate !== undefined ){
+        const formData = new FormData();
+        formData.set('action', 'getGames');
+        formData.set('gameDate', formatDate(this.currentDate));
+
+        axios({
+          method: 'post',
+          url: apiModule.getUrl(),
+          data: formData,
+          responseType: 'json'
+        })
+        .then((response) => {
+          if( response.data.length > 0 ){
+            this.gamesList = response.data;
+          }
+        })
+        .catch(function(error){
+          console.log( error );
+        });
+      }
     },
     getData: function(event){
-      console.log( this.minuteRange );
+      if( this.selectedGameID !== undefined && this.currentDate !== undefined && this.minuteRange.length === 2 ){
+        const formData = new FormData();
+        formData.set('action', "getStats");
+        formData.set('gameID', this.selectedGameID);
+        formData.set('gameDate', formatDate(this.currentDate));
+        formData.set('startTime', this.minuteRange[0]);
+        formData.set('endTime', this.minuteRange[1]);
+
+        axios({
+          method: 'post',
+          url: apiModule.getUrl(),
+          data: formData,
+          responseType: 'json'
+        })
+        .then((response) => {
+          console.log( response );
+        })
+        .catch(function(error){
+          console.log( error );
+        });
+      }
     }
   },
   computed: {
@@ -19,33 +71,42 @@ const vm = new Vue ({
     }
   },
   mounted: function(){
-    const date = new Date();
-    date.setDate( date.getDate() - 1 );
-    this.currentDate = date.toISOString().substr(0,10);
-
+    // const date = new Date();
+    // date.setDate( date.getDate() - 1 );
+    // this.currentDate = date.toISOString().substr(0,10);
+    this.currentDate = '2017-11-08';
     /*
       Initialize time range slider
-     */
-    noUiSlider.create(this.$refs.vueInputSlider, {
-      start: [this.minuteStart, this.minuteEnd],
-      step: 1,
-      connect: true,
-      range: {
-        'min': [0],
-        'max': [48],
-      },
-      pips: {
-        mode: 'steps',
-        density: 3,
-        filter: function( value, type ){
-          return value % 12 ? 2 : 1;
+      */
+      noUiSlider.create(this.$refs.vueInputSlider, {
+        start: [this.minuteStart, this.minuteEnd],
+        step: 1,
+        connect: true,
+        range: {
+          'min': [0],
+          'max': [48],
+        },
+        pips: {
+          mode: 'steps',
+          density: 3,
+          filter: function( value, type ){
+            return value % 12 ? 2 : 1;
+          }
         }
-      }
-    });
+      });
 
-    this.$refs.vueInputSlider.noUiSlider.on('set', (values) => {
-      this.minuteStart = parseInt(values[0]);
-      this.minuteEnd = parseInt(values[1]);
-    });
+      this.$refs.vueInputSlider.noUiSlider.on('set', (values) => {
+        this.minuteStart = parseInt(values[0]);
+        this.minuteEnd = parseInt(values[1]);
+      });
+    }
+  });
+
+  /**
+   * Format date from input to match pattern accepted by the NBA's API
+   * @param  String
+   * @return String
+   */
+   function formatDate(dateString){
+    return dateString.replace(new RegExp('-', 'g'), "");
   }
-})
--- inc/js/main-vue.js
+++ inc/js/main-vue.js
@@ -1,18 +1,68 @@
 'use strict';
 
+var apiModule = function () {
+  var module = {};
+  var url = "http://localhost:8888/api/index.php";
+
+  module.getUrl = function () {
+    return url;
+  };
+
+  return module;
+}();
+
 var vm = new Vue({
   el: '#app',
   data: {
     minuteStart: 0,
     minuteEnd: 48,
-    currentDate: null
+    currentDate: undefined,
+    gamesList: [],
+    selectedGameID: undefined
   },
   methods: {
     getGames: function getGames(event) {
-      console.log(this.currentDate);
+      var _this = this;
+
+      if (this.currentDate !== undefined) {
+        var formData = new FormData();
+        formData.set('action', 'getGames');
+        formData.set('gameDate', formatDate(this.currentDate));
+
+        axios({
+          method: 'post',
+          url: apiModule.getUrl(),
+          data: formData,
+          responseType: 'json'
+        }).then(function (response) {
+          if (response.data.length > 0) {
+            _this.gamesList = response.data;
+          }
+        }).catch(function (error) {
+          console.log(error);
+        });
+      }
     },
     getData: function getData(event) {
-      console.log(this.minuteRange);
+      if (this.selectedGameID !== undefined && this.currentDate !== undefined && this.minuteRange.length === 2) {
+        var formData = new FormData();
+        formData.set('action', "getStats");
+        formData.set('gameID', this.selectedGameID);
+        formData.set('gameDate', formatDate(this.currentDate));
+        formData.set('startTime', this.minuteRange[0]);
+        formData.set('endTime', this.minuteRange[1]);
+
+        axios({
+          method: 'post',
+          url: apiModule.getUrl(),
+          data: formData,
+          responseType: 'json'
+        }).then(function (response) {
+          console.log(response);
+        }).catch(function (error) {
+          console.log(error);
+        });
+      }
     }
   },
   computed: {
@@ -21,15 +71,15 @@ var vm = new Vue({
     }
   },
   mounted: function mounted() {
-    var _this = this;
+    var _this2 = this;
 
-    var date = new Date();
-    date.setDate(date.getDate() - 1);
-    this.currentDate = date.toISOString().substr(0, 10);
-
+    // const date = new Date();
+    // date.setDate( date.getDate() - 1 );
+    // this.currentDate = date.toISOString().substr(0,10);
+    this.currentDate = '2017-11-08';
     /*
       Initialize time range slider
-     */
+      */
     noUiSlider.create(this.$refs.vueInputSlider, {
       start: [this.minuteStart, this.minuteEnd],
       step: 1,
@@ -48,9 +98,18 @@ var vm = new Vue({
     });
 
     this.$refs.vueInputSlider.noUiSlider.on('set', function (values) {
-      _this.minuteStart = parseInt(values[0]);
-      _this.minuteEnd = parseInt(values[1]);
+      _this2.minuteStart = parseInt(values[0]);
+      _this2.minuteEnd = parseInt(values[1]);
     });
   }
 });
+
+/**
+ * Format date from input to match pattern accepted by the NBA's API
+ * @param  String
+ * @return String
+ */
+function formatDate(dateString) {
+  return dateString.replace(new RegExp('-', 'g'), "");
+}
 //# sourceMappingURL=main-vue.js.map
